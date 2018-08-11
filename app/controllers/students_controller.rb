10ef683cfd49ffb7d39fc4cfe7ad4bab82182b04
github username <github email address>
2018-08-11 14:32:38 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
+require "byebug"
+
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -12,6 +14,12 @@ class StudentsController < ApplicationController
   end
 
   def create
+    @new_s = Student.new
+    # byebug
+    @new_s.first_name = params[:first_name]
+    @new_s.last_name = params[:last_name]
+    @new_s.save
+    redirect_to student_path(@new_s)
   end
 
 end
